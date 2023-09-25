8713807ba2ab228303b24fa34b40a127b23c9b48
TheJackal03 <gregorytsak3@gmail.com>
2023-09-25 12:56:37 +0100

    Added the problem's description and one more solution that I figured out. I also added comments for each solution, briefly explaining the solution and their Time Complexity

--- 217-Contains_Duplicate.js
+++ 217-Contains_Duplicate.js
@@ -22,7 +22,9 @@
  */
 
 
-// O(n) Time Complexity
+// O(n) Time Complexity;
+// In this solution i use a map to check if a number previously existed in the array. Basically on each iteration i check if there is a key with the same value and if there  isn't then
+// I add it to the map. 
 var containsDuplicate = function(nums) {
     let numMap = new Map()
     for (let i = 0; i < nums.length; i++){
@@ -32,18 +34,30 @@ var containsDuplicate = function(nums) {
 
     return false;
 };
-
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
 
 
+// O(n) Time Complexity:
+// In this solutionn the exact same logic is used as above with the only difference that i use a set, meaning that there are not any indices therefore when a duplicate is not found, only
+// the VALUE is added to the set and not the value AND the index (compared to the Map structure used above)
 var containsDuplicate2 = function(nums) {
     let numSet = new Set();
     for (const num of nums ){
-        if (numSet.has(nums[i])) return true;
-        numMap.add(nums[i]);
+        if (numSet.has(num)) return true;
+        numSet.add(num);
     } 
 
     return false;
 };
+console.log(containsDuplicate2([1, 2, 3, 5]));
 
-console.log(containsDuplicate([1, 2, 3, 5]));
+
+// O(n) Time Complexity:
+// In this solution a Set is used more efficiently, memory-wise and NOT time-wise. The most known attribute of Sets is that even if they have duplicates, they only keep and make an array with
+// unique values only. So by using this point, after I make a Set with the nums arra, I then check the Sets and the nums sizes. In case there are duplicates in the nums array, then
+// the Set's size would be smaller (different) than the nums array length, since Sets only keep unique values like I said.
+var containsDuplicate3 = function (nums) {
+    let mySet = new Set(nums);
+    return (mySet.size !== nums.length)
+}
+console.log(containsDuplicate3([1, 2, 3, 5]));
