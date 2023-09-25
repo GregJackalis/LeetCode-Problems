5b2ca68b51be353a45eba095f1f9e62aed8b3125
TheJackal03 <gregorytsak3@gmail.com>
2023-09-25 13:00:39 +0100

    I added some comments and re-arranged the format of each solution and its comments

--- two_sum.js
+++ two_sum.js
@@ -29,23 +29,25 @@
  * @return {number[]}
  */
 
- // O(n^2) time Complexity
-// const twoSum = function(nums, target) {
-//     for (let i = 0; i < nums.length; i++) {
-//         for (let j = 0; j < nums.length; j++) {
-//             if (i === j) continue;
-//             if (nums[i] + nums[j] == target) return [i, j]; else continue;
-//         }
-//     }
-//     return [];  // No solution
-// };
-
-// In this solution i used two for loops to iterate trhough every possible number combination till a sum is found that has the same value as the "target"
-
+ // O(n^2) Time Complexity:
+ // In this solution i used two for loops to iterate trhough every possible number combination till a sum is found that has the same value as the "target"
+const twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) continue;
+            if (nums[i] + nums[j] == target) return [i, j]; else continue;
+        }
+    }
+    return [];  // No solution
+};
 
 
-// O(n) Time Complexity
-function twoSum(nums, target) {
+
+// O(n) Time Complexity:
+// In this solution I used a map where the keys are the values of the nums array and the indices are the actual values of each key.
+// Then on each iteration I have a variable that finds the difference between the target and the current value of the nums array.
+// After that I check if there is a KEY in the map with the same value as the difference, if not then i add the current value as a key to the map, with also its index
+const twoSum2 = function (nums, target) {
     const numMap= new Map();
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
@@ -54,9 +56,6 @@ function twoSum(nums, target) {
     }
     return []; // No solution
 }
-// In this solution I used a map where the keys are the values of the nums array and the indices are the actual values of each key.
-// Then on each iteration I have a variable that finds the difference between the target and the current value of the nums array.
-// After that I check if there is a KEY in the map with the same value as the difference, if not then i add the current value as a key to the map, with also its index
 
 // So in case we have an array that's [2,4,6], and the target = 8, tebn at the last iteration the difference would be 8 - 6 = 2, and
 // then its checking if there is a key = 2 in the map, which there is, and in that case the values of those two keys (meaning the indices
