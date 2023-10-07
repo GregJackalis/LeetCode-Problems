1841a903bf9a8d8a11134908a3835fe39bf2278c
TheJackal03 <gregorytsak3@gmail.com>
2023-10-07 20:25:38 +0300

    new problem sovled. 11. Container with water is the title of t he problem: this problem's description was the hardest part of me because the whole water container situation made it look harder than wha t it is. After a while I understood that it needs a more of a 'rectangle approach' and that's what I did by using the Binary Search WITHOU T sorting the array

---
+++ 11-Container_with_Most_Water.js
@@ -0,0 +1,44 @@
+//                                      11. Container with Most Water
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+
+
+// O(n) Time Complexity:
+// **  NOTE: NOT THE BEST PROBLEM DESCRIPTION. I APPROACHED THE PROBLEM AS IN FINDING THE MAX "SUB-RECTANGLE"
+// - I used the Binary Search Method with the only difference that I didn't sorted the array, simply because it's a bar chart so each 
+//   position matters and sorting the array/barchart based on the values would ruin the actual barchart
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    if (height.length <= 2) return Math.min(...height)
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+    while (left < right) {
+        // let formula = (right - left) * (Math.min(height[left], height[right]));
+
+        // same thing as above but simpler to understand. both have very small time differencies
+        let widthVar = right - left;
+        let heightVar = Math.min(height[left], height[right]);
+        let formula = widthVar * heightVar;
+
+        max = Math.max(max, formula);
+
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+    return max;
+};
+
+
+// TESTCASES
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // correct answer: 49
+console.log(maxArea([1,2,4,3])); // correct answer: 4
+console.log(maxArea([1,8,6,2,5,4,8,25,7])); // correct answer: 49
